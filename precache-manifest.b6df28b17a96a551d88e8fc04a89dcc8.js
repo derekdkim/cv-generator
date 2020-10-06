8895353c73559fc38f36b2fa4fe9e9aaf7d619f0
Derek Kim <ddkim1024@gmail.com>
2020-10-05 21:05:39 -0500

    Updates

---
+++ precache-manifest.b6df28b17a96a551d88e8fc04a89dcc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4aa6ab91e1cfcfe81d03913aa1a9bb",
+    "url": "/derekdkim/cv-generator/index.html"
+  },
+  {
+    "revision": "2091f36319985d4ada8e",
+    "url": "/derekdkim/cv-generator/static/css/main.9607d8a9.chunk.css"
+  },
+  {
+    "revision": "d209a07b0cc8fe6e4631",
+    "url": "/derekdkim/cv-generator/static/js/2.7c08d55a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/derekdkim/cv-generator/static/js/2.7c08d55a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2091f36319985d4ada8e",
+    "url": "/derekdkim/cv-generator/static/js/main.fa36f6c3.chunk.js"
+  },
+  {
+    "revision": "cd61a8dd30371356c225",
+    "url": "/derekdkim/cv-generator/static/js/runtime-main.ad584108.js"
+  }
+]);
